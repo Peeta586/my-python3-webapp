6cf9ec9c7e6669fd6a997a53105958b7be534ee7
LSHM <lshm_cqu@163.com>
2017-01-07 12:25:26 +0800

    modified app.py

--- www/app.py
+++ www/app.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-print "start!``"
+#Set logging to print information not error.
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import asyncio,os,json,time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<hl> Hello!</hl>')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop =loop)
+    app.router.add_route('GET','/',index)
+    #assert isinstance(app.make_handler, object)
+    srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
+
